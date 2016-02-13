f75dacec25253440e198c0bcfe354a6c9a817935
sambigon <dzammx@yahoo.com.mx>
2016-02-13 14:52:00 -0600

    MAPA

--- js/API/geolocation.js
+++ js/API/geolocation.js
@@ -11,19 +11,46 @@ var geolocation = {
         geolocation.longitud=position.coords.longitude;
         
         var latYLong = new google.maps.LatLng(geolocation.latitud,geolocation.longitud);
+    
+        var hotel = {lat: 19.0631136, lng: -98.2203158};
+
         //alert('125');
         var opciones={
             zoom: 13,
             center: latYLong,
             mapTypeId: google.maps.MapTypeId.ROADMAP            
         };
-        //alert('126');
         var map = new google.maps.Map(document.getElementById('canvas'), opciones);
+        //alert('126');
+        /*
         var marker = new google.maps.Marker({
            position: latYLong,
            map: map,
            title: "Mi ubicacion"
-        });
+        });*/
+        
+        
+         var directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+          });
+
+          // Set destination, origin and travel mode.
+          var request = {
+            destination: latYLong,
+            origin: hotel,
+            travelMode: google.maps.TravelMode.DRIVING
+          };
+
+          // Pass the directions request to the directions service.
+          var directionsService = new google.maps.DirectionsService();
+          directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              // Display the route on the map.
+              directionsDisplay.setDirections(response);
+            }
+          });
+        
+        
     },
     getPosition:function(){
         //alert("getPosition");
